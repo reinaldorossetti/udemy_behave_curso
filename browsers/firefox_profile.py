4f60fb5661cee06d8471cfb8ca538abcf8986469
Reinaldo Rossetti <reinaldo.rossetti@outlook.com>
2017-05-03 18:01:05 -0300

    Create firefox_profile.py

---
+++ browsers/firefox_profile.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+
+firefox_profile = webdriver.FirefoxProfile()
+firefox_profile.set_preference("network.proxy.type", 1)
+firefox_profile.set_preference("network.proxy.http","ncproxy1") #set your ip
+firefox_profile.set_preference("network.proxy.http_port", 8080) #set your port
+firefox_profile.set_preference("network.proxy.ssl", "ncproxy1")
+firefox_profile.set_preference("network.proxy.ssl_port", 8080)
+driver = webdriver.Firefox(firefox_profile=firefox_profile)
+driver.get("https://www.google.com")
