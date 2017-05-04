d10faab5184612f9da682b781902d92b41ec5225
Reinaldo Rossetti <reinaldo.rossetti@outlook.com>
2017-05-04 09:56:28 -0300

    Add files via upload

---
+++ browsers/ie_proxy_remote.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+
+PROXY = "172.17.241.67:8080"
+
+# Create a copy of desired capabilities object.
+desired_capabilities = webdriver.DesiredCapabilities.INTERNETEXPLORER.copy()
+# Change the proxy properties of that copy.
+desired_capabilities['proxy'] = {
+    "httpProxy":PROXY,
+    "ftpProxy":PROXY,
+    "sslProxy":PROXY,
+    "noProxy":None,
+    "proxyType":"MANUAL",
+    "class":"org.openqa.selenium.Proxy",
+    "autodetect":False
+}
+
+# you have to use remote, otherwise you'll have to code it yourself in python to
+# dynamically changing the system proxy preferences
+driver = webdriver.Remote("http://localhost:4444/wd/hub", desired_capabilities)
+driver.get("http://www.google.com")
