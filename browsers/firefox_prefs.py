992d14507cefcfe6eb9f911e6c8623dac7301e28
reinaldo.rossetti@outlook.com <reiload@gmail.com>
2018-02-03 10:21:53 -0200

    Novo exemplo como utilizar o meu proprio profire no firefox.

---
+++ browsers/firefox_prefs.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from time import sleep
+
+# url mostra como criar um profile
+# https://support.mozilla.org/pt-BR/kb/gerenciador-de-perfis-para-criar-remover-perfis?redirectlocale=en-US&redirectslug=Managing+profiles
+
+dir = "C:\\Users\\reiload\\PycharmProjects\\udemy_my_course\\browsers\\myprofile"
+firefox_profile = webdriver.FirefoxProfile(profile_directory=dir)
+driver = webdriver.Firefox(firefox_profile=firefox_profile)
+
+driver.get("http://reinaldorossetti.blogspot.com.br/")
+
+sleep(30) # somente para esperar, e mostrar para os alunos.
