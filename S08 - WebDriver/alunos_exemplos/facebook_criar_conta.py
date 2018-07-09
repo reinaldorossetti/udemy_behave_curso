104e39a1fcda92456eed5bda32b32979ab35e612
Reinaldo Rossetti <reinaldo.rossetti@outlook.com>
2018-07-09 15:11:30 -0300

    Update facebook_criar_conta.py

--- S08 - WebDriver/alunos_exemplos/facebook_criar_conta.py
+++ S08 - WebDriver/alunos_exemplos/facebook_criar_conta.py
@@ -40,7 +40,6 @@ class CadastrarConta(global_page.Global_Page,
 
 # faz a chamada de uma classe principal ou do step definition do behave.
 driver = webdriver.Firefox()
-driver.implicitly_wait(2)
 driver.get("https://www.facebook.com/")
 test = CadastrarConta(driver)
 dados_user = {
