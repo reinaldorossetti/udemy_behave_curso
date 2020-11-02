9c3e7b8cf38ddd0fd58f086cb76411db41fd1eaf
reinaldorossetti <reinaldo.rossetti@outlook.com>
2020-11-02 18:16:31 -0300

    arrumando os exemplos de headless para chrome e firefox

--- S08 - WebDriver/alunos_exemplos/global_page.py
+++ S08 - WebDriver/alunos_exemplos/global_page.py
@@ -9,13 +9,13 @@ class Global_Page():
     def __init__(self, driver):
         self.driver = driver
 
-    def find_all(self, *locator, timeout=30):
+    def find_all(self, *locator):
         return WebDriverWait(self.driver, timeout).until(presence_of_all_elements_located(*locator))
 
-    def find(self, *locator, timeout=30):
+    def find(self, *locator):
         return WebDriverWait(self.driver, timeout).until(visibility_of_element_located(*locator))
 
-    def find_not(self, *locator, timeout=30):
+    def find_not(self, *locator):
         return WebDriverWait(self.driver, timeout).until(invisibility_of_element_located(*locator))
 
     def tearDown(self):
--- browsers/firefox_prefs.py
+++ browsers/firefox_prefs.py
@@ -4,7 +4,7 @@ from time import sleep
 # url mostra como criar um profile
 # https://support.mozilla.org/pt-BR/kb/gerenciador-de-perfis-para-criar-remover-perfis?redirectlocale=en-US&redirectslug=Managing+profiles
 
-dir = "C:\\Users\\reiload\\PycharmProjects\\udemy_my_course\\browsers\\myprofile"
+dir = "C:\\GitHub\\udemy_my_course\\browsers\\myprofile"
 firefox_profile = webdriver.FirefoxProfile(profile_directory=dir)
 driver = webdriver.Firefox(firefox_profile=firefox_profile)
 
