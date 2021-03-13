8ed11d730fea6f572035ed25e480c2da4c28977f
Reinaldo Rossetti <reinaldo.rossetti@outlook.com>
2021-03-13 16:30:07 -0300

    breve comentario
    breve comentario

--- 08_Selenium_WebDriver_basico/Aula_43/aula_43.py
+++ 08_Selenium_WebDriver_basico/Aula_43/aula_43.py
@@ -1,11 +1,17 @@
 from selenium import webdriver
 
+# instancia e carrega a nossa pagina.
 driver = webdriver.Chrome()
-
 driver.get("https://automacaocombatista.herokuapp.com/users/new")
+
+# funcao para deixar a tela cheia.
 driver.fullscreen_window()
 
+# tempo maximo para ler os elementos da pagina, isso na leitura abertura da pagina,
+# nao eh timeout dos elementos.
 driver.set_page_load_timeout(10)
+
+# exemplos de elementos de busca de forma explicita.
 driver.find_element_by_id("user_name")
 driver.find_element_by_class_name("new_user")
 driver.find_element_by_css_selector("#user_lastname")
